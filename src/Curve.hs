51b0321888dfb8cdf480a59a5eb42ac63e2430b5
Christian Fischer <christian@chfi.se>
2016-11-18 12:04:44 +0100

    Curves die and stop moving upon self-collision

--- src/Curve.hs
+++ src/Curve.hs
@@ -2,22 +2,32 @@
 {-# LANGUAGE RecordWildCards #-}
 
 
-module Curve where
+module Curve
+  ( Curve(..)
+  , createCurve
+  , curveWire
+  , drawCurve
+  ) where
 
+
+import Debug.Trace (trace)
 
 import           Data.List.NonEmpty (NonEmpty ((:|)), cons)
 import qualified Data.List.NonEmpty as NE
 import           Linear.Metric      (distance)
-import           Linear.V2          (V2, angle)
+import           Linear.V2          (V2(..), angle)
 import           Prelude                  hiding (id, (.))
 import qualified Data.Set as Set
 
 
-import Control.Wire (HasTime, Wire, (.))
+import Control.Wire (HasTime, Wire, (.), mkGen_, mkConst)
 import FRP.Netwire
 
 
-import Graphics (Color, rgbColor)
+import qualified SDL as SDL
+
+
+import Graphics (Color, rgbColor, drawCircleAt)
 import Input
 
   -- TODO should be configurable somewhere
@@ -29,11 +39,22 @@ type Tail = NonEmpty (V2 Double)
 
 
 data Curve = Curve { cHead  :: Head
-                     , cTail  :: Tail
-                     , cDist  :: Double
-                     , cAlive :: Bool
-                     , cColor :: Color
-                     }
+                   , cTail  :: Tail
+                   , cDist  :: Double
+                   , cAlive :: Bool
+                   , cColor :: Color
+                   }
+
+-- type CurveWire = Wire s e IO PlayerInput Curve
+
+createCurve :: Head -> Color -> Curve
+createCurve head color =
+  Curve { cHead = head
+        , cTail = head :| []
+        , cDist = curveRadius
+        , cAlive = True
+        , cColor = color
+        }
 
 
 
@@ -61,35 +82,55 @@ selfColWire = proc p -> do
   returnA -< val
 
 
-plWire :: HasTime t s => Curve -> Wire s () IO CurveInput Curve
-plWire p = proc keys -> do
-  pH <- plPosWire (cHead p) -< keys
-  rec pT <- delay (cTail p) -< pT'
-      let p' = curveExtendTail p { cHead = pH, cTail = pT }
-          pT' = cTail p'
-  alive <- (arr not) . selfColWire -< p'
-  returnA -< p' { cAlive = alive }
+
+killableWire :: HasTime t s => Wire s () IO (PlayerInput, Bool) (V2 Double)
+killableWire = proc (input, alive) -> do
+  if alive
+    then do posWire (V2 100 100) 0 -< input
+    else 0 -< ()
 
 
+curveWire :: HasTime t s => Curve -> Double -> Wire s () IO PlayerInput Curve
+curveWire c initAng = proc keys -> do
 
-plPosWire :: HasTime t s => (V2 Double) -> Wire s () IO CurveInput (V2 Double)
-plPosWire init = proc keys -> do
-  -- pos <- integral init . (angvel >>> polarVel) -< keys
-  pos <- integral init . (angWire >>> polarVel) -< keys
+  rec cT <- delay (cTail c) -< cT'
+      a' <- delay True -< alive
+      cH <- integral 0 . killableWire -< (keys, a')
+
+      let c' = curveExtendTail c { cHead = cH, cTail = cT }
+          cT' = cTail c'
+
+      alive <- ((arr not) . selfColWire) -< c'
+
+  returnA -< c' { cAlive = alive }
+
+
+posWire :: HasTime t s => (V2 Double) -> Double -> Wire s () IO PlayerInput (V2 Double)
+posWire initPos initAng = proc keys -> do
+  pos <- ((angWire initAng) >>> polarVel) -< keys
   returnA -< pos
 
-angWire :: HasTime t s => Double -> Wire s () IO CurveInput Double
-angWire da = proc keys -> do
+
+angWire :: HasTime t s => Double -> Wire s () IO PlayerInput Double
+angWire init = proc keys -> do
+  va <-  (-3) . when (isDown KeyLeft)
+     <|> 3 . when (isDown KeyRight)
+     <|> 0
+          -< keys
+  integral init -< va
+
+
+angWire' :: HasTime t s => Double -> Wire s () IO PlayerInput Double
+angWire' da = proc keys -> do
   -- pretty ugly --- need to create constant arrows to feed the speed, since it's a double.
   va <-  (arr (const (-da))) . when (isDown KeyLeft)
      <|> (arr (const   da )) . when (isDown KeyRight)
      <|> 0
           -< keys
-
   integral 0 -< va
 
 
--- angvel :: HasTime t s => Wire s () IO CurveInput Double
+-- angvel :: HasTime t s => Wire s () IO PlayerInput Double
 -- angvel = proc keys -> do
 --   va <-  (-3) . when (isDown KeyLeft)
 --      <|>   3  . when (isDown KeyRight)
@@ -104,3 +145,12 @@ polarVel :: HasTime t s => Wire s () IO Double (V2 Double)
 polarVel = proc ang -> do
   v <- arr angle -< ang
   returnA -< (v * 100)
+
+
+-- should probably move Color into a separate module,
+-- or this into a separate Rendering module,
+-- either way it shouldn't be in Curve
+drawCurve :: SDL.Renderer -> Curve -> IO ()
+drawCurve r Curve{..} = do
+  drawCircleAt r cColor cHead
+  mapM_ (drawCircleAt r cColor) cTail
